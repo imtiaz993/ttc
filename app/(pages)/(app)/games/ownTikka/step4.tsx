e7a7552faece74e8ed517801ec15962bfb6fabcc
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-05-15 23:21:57 +0500

    fix

--- app/(pages)/(app)/games/ownTikka/step4.tsx
+++ app/(pages)/(app)/games/ownTikka/step4.tsx
@@ -16,26 +16,12 @@ const OwnTikkaStep4 = () => {
       <GameStepper showPrev={false} />
       <div className="h-full pt-16 px-4 flex flex-col justify-start items-center bg-[#FFF8E7]">
         <div className="flex justify-center items-center mb-16">
-          <img
-            src="/images/own-tikka-final.png"
-           
-
-            
-            
-            
-            alt=""
-            className="w-80"
-          />
+          <img src="/images/own-tikka-final.png" alt="" className="w-80" />
         </div>
         <div className="w-full flex items-start mb-10">
           <div>
             <img
               src={`/images/${userData.char}.png`}
-             
-
-              
-              
-              
               alt=""
               className="w-11 rounded-lg"
             />
@@ -46,36 +32,22 @@ const OwnTikkaStep4 = () => {
           </p>
         </div>
         <div className="w-full grid grid-cols-2 gap-4">
-          <button className="border border-black bg-transparent rounded font-semibold flex justify-center py-3 w-full">
-            <img
-              src="/icons/download.svg"
-             
-
-              
-              
-              
-              alt=""
-              className="w-6 mr-2"
-              onClick={() => {
-                next();
-              }}
-            />
+          <button
+            onClick={() => {
+              next();
+            }}
+            className="border border-black bg-transparent rounded font-semibold flex justify-center py-3 w-full"
+          >
+            <img src="/icons/download.svg" alt="" className="w-6 mr-2" />
             Download
           </button>
-          <button className="text-[#FFF8E7] font-semibold rounded flex justify-center bg-black border border-black py-3 w-full">
-            <img
-              src="/icons/share.svg"
-             
-
-              
-              
-              
-              alt=""
-              className="w-6 mr-2"
-              onClick={() => {
-                next();
-              }}
-            />
+          <button
+            onClick={() => {
+              next();
+            }}
+            className="text-[#FFF8E7] font-semibold rounded flex justify-center bg-black border border-black py-3 w-full"
+          >
+            <img src="/icons/share.svg" alt="" className="w-6 mr-2" />
             Share
           </button>
         </div>
