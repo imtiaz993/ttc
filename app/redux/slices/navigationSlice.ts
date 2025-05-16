04add5131f42bc272a8e1fc6c0c89c717bec067a
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-05-16 21:14:50 +0500

    fix

--- app/redux/slices/navigationSlice.ts
+++ app/redux/slices/navigationSlice.ts
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const navigationSlice = createSlice({
   name: "navigation",
   initialState: {
-    step: 19,
+    step: 1,
   },
   reducers: {
     nextStep: (state) => {
