90e54174df20532338da3711295712c01e6422f6
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-05-18 19:31:50 +0500

    build fixed v9

--- app/(pages)/(app)/page.tsx
+++ app/(pages)/(app)/page.tsx
@@ -8,26 +8,26 @@ import Welcome from "./components/welcome";
 import ScratchStep1 from "./games/scratch/step1";
 import ScratchStep2 from "./games/scratch/step2";
 
-import SareeStep1 from "./games/saree/step1";
-import SareeStep2 from "./games/saree/step2";
-import SareeStep3 from "./games/saree/step3";
+// import SareeStep1 from "./games/saree/step1";
+// import SareeStep2 from "./games/saree/step2";
+// import SareeStep3 from "./games/saree/step3";
 
-import SpotTikkaStep1 from "./games/spotTikka/step1";
-import SpotTikkaStep2 from "./games/spotTikka/step2";
+// import SpotTikkaStep1 from "./games/spotTikka/step1";
+// import SpotTikkaStep2 from "./games/spotTikka/step2";
 
-import WordsStep1 from "./games/words/step1";
-import WordsStep2 from "./games/words/step2";
+// import WordsStep1 from "./games/words/step1";
+// import WordsStep2 from "./games/words/step2";
 
-import PuzzleStep1 from "./games/puzzle/step1";
-import PuzzleStep2 from "./games/puzzle/step2";
-import PuzzleStep3 from "./games/puzzle/step3";
-import PuzzleStep4 from "./games/puzzle/step4";
-import PuzzleStep5 from "./games/puzzle/step5";
+// import PuzzleStep1 from "./games/puzzle/step1";
+// import PuzzleStep2 from "./games/puzzle/step2";
+// import PuzzleStep3 from "./games/puzzle/step3";
+// import PuzzleStep4 from "./games/puzzle/step4";
+// import PuzzleStep5 from "./games/puzzle/step5";
 
-import OwnTikkaStep1 from "./games/ownTikka/step1";
-import OwnTikkaStep2 from "./games/ownTikka/step2";
-import OwnTikkaStep3 from "./games/ownTikka/step3";
-import OwnTikkaStep4 from "./games/ownTikka/step4";
+// import OwnTikkaStep1 from "./games/ownTikka/step1";
+// import OwnTikkaStep2 from "./games/ownTikka/step2";
+// import OwnTikkaStep3 from "./games/ownTikka/step3";
+// import OwnTikkaStep4 from "./games/ownTikka/step4";
 
 import Feedback from "./components/feedback";
 import Thankyou from "./components/thankyou";
@@ -86,35 +86,35 @@ export default function Home() {
 
   const ScratchGame = [<ScratchStep1 />, <ScratchStep2 />];
 
-  const SareeGame = [<SareeStep1 />, <SareeStep2 />, <SareeStep3 />];
+  // const SareeGame = [<SareeStep1 />, <SareeStep2 />, <SareeStep3 />];
 
-  const SpotTikka = [<SpotTikkaStep1 />, <SpotTikkaStep2 />];
+  // const SpotTikka = [<SpotTikkaStep1 />, <SpotTikkaStep2 />];
 
-  const WordsGame = [<WordsStep1 />, <WordsStep2 />];
+  // const WordsGame = [<WordsStep1 />, <WordsStep2 />];
 
-  const PuzzleGame = [
-    <PuzzleStep1 />,
-    <PuzzleStep2 />,
-    <PuzzleStep3 />,
-    <PuzzleStep4 />,
-    <PuzzleStep5 />,
-  ];
+  // const PuzzleGame = [
+  //   <PuzzleStep1 />,
+  //   <PuzzleStep2 />,
+  //   <PuzzleStep3 />,
+  //   <PuzzleStep4 />,
+  //   <PuzzleStep5 />,
+  // ];
 
-  const OwnTikkaGame = [
-    <OwnTikkaStep1 />,
-    <OwnTikkaStep2 />,
-    <OwnTikkaStep3 />,
-    <OwnTikkaStep4 />,
-  ];
+  // const OwnTikkaGame = [
+  //   <OwnTikkaStep1 />,
+  //   <OwnTikkaStep2 />,
+  //   <OwnTikkaStep3 />,
+  //   <OwnTikkaStep4 />,
+  // ];
 
   const components = [
     <Welcome playMusic={playMusic} />,
     ...ScratchGame,
-    ...SareeGame,
-    ...SpotTikka,
-    ...WordsGame,
-    ...PuzzleGame,
-    ...OwnTikkaGame,
+    // ...SareeGame,
+    // ...SpotTikka,
+    // ...WordsGame,
+    // ...PuzzleGame,
+    // ...OwnTikkaGame,
     <Feedback />,
     <Thankyou />,
   ];
