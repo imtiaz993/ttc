eb66ba9844d79d7419c2a30257f9c4e383d15ad5
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-05-18 19:30:36 +0500

    build fixed v8

--- app/(pages)/(app)/components/scratchCard.tsx
+++ app/(pages)/(app)/components/scratchCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useRef, useEffect, useState } from "react";
 import confetti from "canvas-confetti";
 import Lottie from "react-lottie";
--- app/(pages)/(app)/games/scratch/step1.tsx
+++ app/(pages)/(app)/games/scratch/step1.tsx
@@ -1,14 +1,18 @@
+import dynamic from "next/dynamic";
 import { useState } from "react";
 import Menu from "../../components/menu";
 import GameStepper from "../../components/gameStepper";
-import ScratchCard from "../../components/scratchCard";
+
+const ScratchCard = dynamic(() => import("../../components/scratchCard"), {
+  ssr: false,
+});
 
 const ScratchStep1 = () => {
   const [isRevealed, setIsRevealed] = useState(false);
   return (
     <>
       <Menu />
-      <GameStepper iswhite  />
+      <GameStepper iswhite />
       <ScratchCard isRevealed={isRevealed} setIsRevealed={setIsRevealed} />
     </>
   );
