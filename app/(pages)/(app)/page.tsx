5d5619ffc998bc6b70a0e174cb70f627205f177e
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-05-18 19:29:08 +0500

    build fixed v7

--- app/(pages)/(app)/page.tsx
+++ app/(pages)/(app)/page.tsx
@@ -8,9 +8,9 @@ import Welcome from "./components/welcome";
 import ScratchStep1 from "./games/scratch/step1";
 import ScratchStep2 from "./games/scratch/step2";
 
-// import SareeStep1 from "./games/saree/step1";
-// import SareeStep2 from "./games/saree/step2";
-// import SareeStep3 from "./games/saree/step3";
+import SareeStep1 from "./games/saree/step1";
+import SareeStep2 from "./games/saree/step2";
+import SareeStep3 from "./games/saree/step3";
 
 // import SpotTikkaStep1 from "./games/spotTikka/step1";
 // import SpotTikkaStep2 from "./games/spotTikka/step2";
@@ -86,7 +86,7 @@ export default function Home() {
 
   const ScratchGame = [<ScratchStep1 />, <ScratchStep2 />];
 
-  // const SareeGame = [<SareeStep1 />, <SareeStep2 />, <SareeStep3 />];
+  const SareeGame = [<SareeStep1 />, <SareeStep2 />, <SareeStep3 />];
 
   // const SpotTikka = [<SpotTikkaStep1 />, <SpotTikkaStep2 />];
 
@@ -110,7 +110,7 @@ export default function Home() {
   const components = [
     <Welcome playMusic={playMusic} />,
     ...ScratchGame,
-    // ...SareeGame,
+    ...SareeGame,
     // ...SpotTikka,
     // ...WordsGame,
     // ...PuzzleGame,
