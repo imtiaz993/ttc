1a73dc31b64467e8ddc18d722d22d263f7874e7b
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-06-02 14:43:35 +0500

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
 import "./globals.css";
 import Wrapper from "./Wrapper";
 
-// export const metadata = {
-//   openGraph: {
-//     siteName: "Timeless Ticket Tales",
-//     images: [
-//       {
-//         title: "Timeless Ticket Tales",
-//         description: "",
-//         image: "/opengraph-image.jpg",
-//         url: "https://ttc-delta.vercel.app/opengraph-image.jpg",
-//         alt: "",
-//       },
-//     ],
-//     locale: "en_US",
-//     type: "website",
-//   },
-// };
+export const metadata = {
+  openGraph: {
+    siteName: "Timeless Ticket Tales",
+    images: [
+      {
+        title: "Timeless Ticket Tales",
+        description: "",
+        image: "https://ttc-delta.vercel.app/opengraph-image.jpg",
+        url: "https://ttc-delta.vercel.app/opengraph-image.jpg",
+        alt: "",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -26,10 +26,6 @@ export default function RootLayout({ children }) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0"
         />
-        <meta property="og:title" content="Timeless Ticket Tales" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://ttc-delta.vercel.app/opengraph-image.jpg" />
-        <meta property="og:url" content="https://ttc-delta.vercel.app/opengraph-image.jpg" />
       </head>
       <body className="min-h-dvh max-h-dvh flex justify-center items-center overflow-hidden">
         <Wrapper>{children}</Wrapper>
