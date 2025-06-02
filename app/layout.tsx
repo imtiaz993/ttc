c892733b4b1139b00c0dc3c131876f9d58aa9425
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-06-02 16:06:42 +0500

    fixex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import "./globals.css";
 import Wrapper from "./Wrapper";
 
+const baseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://ttc-delta.vercel.app";
+
 export const metadata = {
   openGraph: {
     url: "https://ttc-delta.vercel.app",
     type: "website",
     images: [
       {
-        url: "/opengraph-image.jpg",
+        url: `${baseUrl}/opengraph-image.jpg`, // Absolute URL
         width: 1200,
         height: 630,
-        alt: "",
+        alt: "Website OG Image", // Add a descriptive alt text
       },
     ],
   },
