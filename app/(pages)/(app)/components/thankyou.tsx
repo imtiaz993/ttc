d10a92074cfbc2bbd6189d57f12da2b2518292c3
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-06-02 15:57:58 +0500

    fixed max

--- app/(pages)/(app)/components/thankyou.tsx
+++ app/(pages)/(app)/components/thankyou.tsx
@@ -14,7 +14,7 @@ const Thankyou = () => {
         </p>
       </div>
       <img src="/icons/logo.svg" alt="" className="w-24 mb-1 xs:mb-6" />
-      <p className="text-center text-[#D02E01] text-[11px] mb-2 xs:mb-3 px-9 font-manrope">
+      <p className="text-center text-[#D02E01] text-[11px] mb-1 xs:mb-3 px-9 font-manrope">
         This exhibition and digital experience have been supported by the Bank
         of America
       </p>
@@ -23,57 +23,57 @@ const Thankyou = () => {
           <img src="/icons/bofa-bank.svg" alt="" className="w-full" />
         </div>
 
-        <p className="text-center text-[#D02E01] text-[11px] mt-2 xs:mt-5 font-manrope">
+        <p className="text-center text-[#D02E01] text-[11px] mt-1 xs:mt-5 font-manrope">
           All images courtesy of the Museum of Art & Photography (MAP)
           Collection, Bengaluru
         </p>
         <div className="mt-2 xs:mt-5 font-manrope">
-          <div className="flex justify-between items-center mb-2 xs:mb-3">
+          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
             <p className="text-[11px] text-[#D02E01]">Concept</p>
             <p className="text-[#D02E01] font-semibold text-[11px]">
               Shyamli Singbal
             </p>
           </div>
-          <div className="flex justify-between items-center mb-2 xs:mb-3">
+          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
             <p className="text-[11px] text-[#D02E01]">
               Narrative Design & Scripting
             </p>
-            <p className="text-[#D02E01] text-[11px] font-semibold">
+            <p className="text-[#D02E01] text-[11px] font-semibold whitespace-nowrap">
               Hina Siddiqui
             </p>
           </div>
           <div className="flex justify-between items-strat mb-3">
             <p className="text-[11px] text-[#D02E01]">Creative Asset Design</p>
-            <p className="text-[#D02E01] font-semibold text-right text-[11px]">
+            <p className="text-[#D02E01] font-semibold text-right text-[11px] whitespace-nowrap">
               Alexis Fu, Kusuma S,
               <br /> Cibal Prems
             </p>
           </div>
-          <div className="flex justify-between items-center mb-2 xs:mb-3">
+          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
             <p className="text-[11px] text-[#D02E01]">Design Consultant</p>
             <p className="text-[#D02E01] font-semibold text-[11px]">
               Nilay Gomkale
             </p>
           </div>
-          <div className="flex justify-between items-center mb-2 xs:mb-3">
+          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
             <p className="text-[11px] text-[#D02E01]">Digitization</p>
             <p className="text-[#D02E01] font-semibold text-right text-[11px]">
               Bela Bahirat, Ramesha N S
             </p>
           </div>
-          <div className="flex justify-between items-center mb-2 xs:mb-3">
+          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
             <p className="text-[11px] text-[#D02E01]">Tech Support</p>
             <p className="text-[#D02E01] font-semibold text-[11px]">
               Mohammad Imran
             </p>
           </div>
-          <div className="flex justify-between items-center mb-2 xs:mb-3">
+          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
             <p className="text-[11px] text-[#D02E01]">Development</p>
             <p className="text-[#D02E01] font-semibold text-[11px]">
               United Monks
             </p>
           </div>
-          <div className="flex justify-between items-center mb-2 xs:mb-3">
+          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
             <p className="text-[11px] text-[#D02E01]">
               UI & UX Design & Animation
             </p>
