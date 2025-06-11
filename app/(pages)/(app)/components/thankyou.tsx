1a22e3a348fff3a9e135b9a1b317afb10075dcfb
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-06-11 16:16:11 +0500

    added new credit screen

--- app/(pages)/(app)/components/thankyou.tsx
+++ app/(pages)/(app)/components/thankyou.tsx
@@ -2,90 +2,76 @@ const Thankyou = () => {
   return (
     <div
       id="screen-32"
-      className="flex flex-col justify-center items-center border-transparent h-dvh px-12 max-h-[1000px]"
-      style={{
-        backgroundImage: "url('/images/welcome-frame.png')",
-        backgroundSize: "contain",
-        backgroundRepeat: "round",
-      }}
+      className="flex flex-col justify-center items-center border-transparent h-dvh px-7 xs:px-10 py-[60px] max-h-[1000px] bg-[#B9D7B0]"
     >
-      <div className="px-9">
-        <p className="text-[#D02E01] text-sm font-medium mb-2 text-center font-lora">
-          Thanks for visiting!
+      <div className="w-full">
+        <p className="text-[#D02E01] text-xl font-medium mb-2 text-center font-lora">
+          Thank you for playing
         </p>
-      </div>
-      <img src="/icons/logo.svg" alt="" className="w-24 mb-1 xs:mb-6" />
-      <p className="text-center text-[#D02E01] text-[11px] mb-1 xs:mb-3 px-9 font-manrope">
-        This exhibition and digital experience have been supported by the Bank
-        of America
-      </p>
-      <div className="w-full px-7">
         <div className="flex justify-center">
-          <img src="/icons/bofa-bank.svg" alt="" className="w-full" />
+          <img src="/icons/logo.svg" alt="" className="w-44 xs:w-52 mb-6" />
         </div>
-
-        <p className="text-center text-[#D02E01] text-[11px] mt-1 xs:mt-5 font-manrope">
-          All images courtesy of the Museum of Art & Photography (MAP)
-          Collection, Bengaluru
-        </p>
-        <div className="mt-2 xs:mt-5 font-manrope">
-          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
-            <p className="text-[11px] text-[#D02E01]">Concept</p>
-            <p className="text-[#D02E01] font-semibold text-[11px]">
-              Shyamli Singbal
-            </p>
+        <div className="text-[#D02E01] text-xs font-manrope">
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">Concept</p>
+            <p className="font-semibold">Shyamli Singbal</p>
           </div>
-          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
-            <p className="text-[11px] text-[#D02E01]">
-              Narrative Design & Scripting
-            </p>
-            <p className="text-[#D02E01] text-[11px] font-semibold whitespace-nowrap">
-              Hina Siddiqui
-            </p>
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">Narrative Design & Scripting</p>
+            <p className="font-semibold whitespace-nowrap">Hina Siddiqui</p>
           </div>
           <div className="flex justify-between items-strat mb-3">
-            <p className="text-[11px] text-[#D02E01]">Creative Asset Design</p>
-            <p className="text-[#D02E01] font-semibold text-right text-[11px] whitespace-nowrap">
+            <p className="font-light">Design Support</p>
+            <p className="font-semibold text-right text-xs whitespace-nowrap">
               Alexis Fu, Kusuma S,
-              <br /> Cibal Prems
+              <br />
+              Cibal Prems, Madhur Arora
             </p>
           </div>
-          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
-            <p className="text-[11px] text-[#D02E01]">Design Consultant</p>
-            <p className="text-[#D02E01] font-semibold text-[11px]">
-              Nilay Gomkale
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">Design Consultant</p>
+            <p className="font-semibold">Nilay Gomkale</p>
+          </div>
+
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">Creative Editor</p>
+            <p className="font-semibold text-right text-xs">
+              Shilpa Vijayakrishnan
             </p>
           </div>
-          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
-            <p className="text-[11px] text-[#D02E01]">Digitization</p>
-            <p className="text-[#D02E01] font-semibold text-right text-[11px]">
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">Digitization</p>
+            <p className="font-semibold text-right text-xs">
               Bela Bahirat, Ramesha N S
             </p>
           </div>
-          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
-            <p className="text-[11px] text-[#D02E01]">Tech Support</p>
-            <p className="text-[#D02E01] font-semibold text-[11px]">
-              Mohammad Imran
-            </p>
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">Tech Support</p>
+            <p className="font-semibold">Mohammad Imran</p>
           </div>
-          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
-            <p className="text-[11px] text-[#D02E01]">Development</p>
-            <p className="text-[#D02E01] font-semibold text-[11px]">
-              United Monks
-            </p>
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">Animations</p>
+            <p className="font-semibold">Jason Azyu</p>
           </div>
-          <div className="flex justify-between items-center mb-1.5 xs:mb-3">
-            <p className="text-[11px] text-[#D02E01]">
-              UI & UX Design & Animation
-            </p>
-            <p className="text-[#D02E01] font-semibold text-[11px]">
-              Sensoryplus
-            </p>
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">Development</p>
+            <p className="font-semibold">United Monks</p>
           </div>
-          <div className="flex justify-center mt-2 xs:mt-6">
-            <img src="/images/map-logo.png" alt="" className="w-16" />
+          <div className="flex justify-between items-center mb-2.5">
+            <p className="font-light">UI & UX Design</p>
+            <p className="font-semibold">Sensoryplus</p>
           </div>
         </div>
+      </div>
+      <div className="mt-10 w-full">
+        <p className="text-center text-[#D02E01] text-xs font-manrope font-light">
+          All images courtesy of the Museum of Art & Photography (MAP)
+          Collection, Bengaluru
+        </p>
+      </div>
+      <div className="w-full mt-6 flex justify-between items-center">
+        <img src="/icons/bofa-bank.svg" alt="" className="w-52" />
+        <img src="/images/map-logo.png" alt="" className="w-16" />
       </div>
     </div>
   );
