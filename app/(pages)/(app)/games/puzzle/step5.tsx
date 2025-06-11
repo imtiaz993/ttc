48b57c4728cf9bf0cd61be2988891f80e4930c82
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-06-11 10:56:13 +0500

    updated cloud

--- app/(pages)/(app)/games/puzzle/step5.tsx
+++ app/(pages)/(app)/games/puzzle/step5.tsx
@@ -1,10 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import Menu from "../../components/menu";
-import { useEffect, useState } from "react";
-import {
-  resetStepperProps,
-  setStepperProps,
-} from "../../../../redux/slices/progressSlice";
+import { useState } from "react";
 import SwipeOverlay from "../../components/swipeOverlay";
 import { openOverlay } from "../../../../redux/slices/userSlice";
 import { useInactivity } from "../../../../hooks/useInactivity";
@@ -17,7 +13,7 @@ const PuzzleStep5 = () => {
   const [overlay, setOverlay] = useState(false);
   const displayOverlay = () => dispatch(openOverlay());
   useInactivity({
-    time: 20000,
+    time: 200000000,
     onInactivity: () => {
       setOverlay(true);
       displayOverlay();
@@ -45,55 +41,48 @@ const PuzzleStep5 = () => {
             <p className="mt-1 text-xs font-medium text-center">You</p>
           </div>
           <p className="ml-4 font-medium w-[calc(100%-46px)]">
-            Tickets here, tickets there, tickets tickets everywhere!
-            <br />
-            <br />
-            Let’s go fishing!
-            <br />
-            Can you help find the catch of the day?
+            The more I look, the more I am surrounded by tikas!
           </p>
         </div>
         <div className="relative w-full">
           <div
-            className="w-[148px] absolute left-0 -top-12 fade-in opacity-0"
-            style={{ animationDelay: "1s", animationFillMode: "forwards" }}
+            className="w-[148px] absolute left-1 top-0 fade-in opacity-0"
+            style={{ animationDelay: "0.5s", animationFillMode: "forwards" }}
           >
-            <p className="py-3 px-4 text-xs absolute text-white z-10">
+            <p className="py-3 px-4 text-xs absolute z-10">
               Spot a pair of winged mermaids. . .
             </p>
-            <img className="relative mt-0.5" src="/icons/cloud.svg" alt="" />
+            <img className="relative mt-0.5 w-[148px]" src="/icons/cloud.svg" alt="" />
           </div>
 
           <div
-            className="w-[148px] absolute -right-2 top-24 fade-in opacity-0"
-            style={{ animationDelay: "2s", animationFillMode: "forwards" }}
+            className="w-[148px] absolute right-2 top-16 fade-in opacity-0"
+            style={{ animationDelay: "1.5s", animationFillMode: "forwards" }}
           >
-            <p className="py-3 px-4 text-xs absolute text-white z-10">
+            <p className="py-4 px-4 text-xs absolute z-10">
               Did you see a pair of black cats yet?
             </p>
-            <img className="relative mt-1" src="/icons/cloud.svg" alt="" />
+            <img className="relative mt-1 w-[148px]" src="/icons/cloud.svg" alt="" />
           </div>
           <div
-            className="w-[151px] absolute left-8 -bottom-16 fade-in opacity-0"
-            style={{ animationDelay: "3s", animationFillMode: "forwards" }}
+            className="w-[151px] absolute left-9 -bottom-10 fade-in opacity-0"
+            style={{ animationDelay: "2.5s", animationFillMode: "forwards" }}
           >
-            <p className="py-3 px-4 text-xs absolute bottom-0 text-white z-10">
+            <p className="py-6 px-4 text-xs absolute bottom-0 z-10">
               Keep safe distance! A family of sharks swims underwater..
             </p>
-            <img className="relative" src="/icons/cloud-2.svg" alt="" />
+            <img className="relative w-[151px]" src="/icons/cloud-2.svg" alt="" />
           </div>
           <div
-            className="w-[133px] absolute -right-2 -bottom-24 xs:-bottom-32 fade-in opacity-0"
-            style={{ animationDelay: "4s", animationFillMode: "forwards" }}
+            className="w-[133px] absolute right-2 -bottom-20 xs:-bottom-32 fade-in opacity-0"
+            style={{ animationDelay: "3.5s", animationFillMode: "forwards" }}
           >
-            <p className="py-3 px-4 bottom-0 text-xs absolute text-white z-10">
+            <p className="py-5 px-4 bottom-0 text-xs absolute z-10">
               What a feast! Find as many items one could eat!
             </p>
-            <img className="relative mb-1" src="/icons/cloud-2.svg" alt="" />
+            <img className="relative mb-1 w-[133px]" src="/icons/cloud-2.svg" alt="" />
           </div>
-          <div className="flex justify-center items-center">
-            <img src="/images/manchester.png" alt="" className="w-40" />
-          </div>
+          <div className="flex justify-center items-center mx-auto w-40 h-[202px]"></div>
         </div>
         <div className="h-28"></div>
       </div>
